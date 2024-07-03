679731ea7f37264c31d115974f365f52369c5841
Dominic Strybny <Dominic.Strybny@gmail.com>
2024-07-03 07:38:46 +0200

    new layout

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-# Weitere Routen und Funktionalitäten hier einfügen
+@app.route("/about", methods=['GET', 'POST'])    
+def about():
+    return render_template("about.html")
 
-if __name__ == '__main__':
+@app.route("/app", methods=['GET', 'POST'])
+def ctw_app():
+    if request.method == 'POST':
+        pass
+    return render_template("app.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
